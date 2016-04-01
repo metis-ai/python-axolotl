3e16b2d7b7e0c91c2b3547e0db35ed2b28b5060b
ericmarcos <ericmarcos.p@gmail.com>
2016-04-02 01:35:14 +0200

    fixed decoding when image is received

--- axolotl/sessioncipher.py
+++ axolotl/sessioncipher.py
@@ -85,7 +85,10 @@ class SessionCipher:
         self.sessionStore.storeSession(self.recipientId, self.deviceId, sessionRecord)
 
         if sys.version_info >= (3,0) and textMsg:
-            return plaintext.decode()
+            try:
+                return plaintext.decode()
+            except UnicodeDecodeError as e:
+                return plaintext.decode('latin-1')
         return plaintext
 
     def decryptPkmsg(self, ciphertext, textMsg=True):
@@ -103,7 +106,10 @@ class SessionCipher:
             self.preKeyStore.removePreKey(unsignedPreKeyId)
 
         if sys.version_info >= (3, 0) and textMsg:
-            return plaintext.decode()
+            try:
+                return plaintext.decode()
+            except UnicodeDecodeError as e:
+                return plaintext.decode('latin-1')
         return plaintext
 
     def decryptWithSessionRecord(self, sessionRecord, cipherText):
